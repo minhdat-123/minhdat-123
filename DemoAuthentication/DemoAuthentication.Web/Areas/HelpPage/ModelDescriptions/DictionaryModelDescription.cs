e098aafeea6064fde43b3c12dac35fd604861064
nguyen minh dat <dat.nguyen@truecommerce.com>
2023-07-05 14:07:21 +0700

    test git hub
    test git hub

---
+++ DemoAuthentication/DemoAuthentication.Web/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace DemoAuthentication.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ DemoAuthentication/DemoAuthentication.Web/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace DemoAuthentication.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
